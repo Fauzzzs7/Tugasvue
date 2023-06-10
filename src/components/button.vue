9d501255042fedf2bb6656a1bfff880be4b42c63
Fauzzzs7 <fauzsyabana68@gmail.com>
2023-06-10 19:46:52 +0700

    tugas vue

---
+++ src/components/button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button type="button" class="btn" :class="[props.class]">
+    {{ props.text }}
+  </button>
+</template>
+<script setup>
+const props = defineProps({
+  text: String,
+  class: String,
+});
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HeroSection from "../components/herosection.vue";
+import Navbar from "../components/navbar.vue";
+</script>
+
+<template>
+  <div class="flex">
+    <Navbar />
+    <main>
+      <HeroSection />
+    </main>
+  </div>
+</template>
